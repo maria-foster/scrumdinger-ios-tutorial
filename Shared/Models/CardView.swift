0f5eac5235f4504afc248982430f33f5041f487e
Maria Foster <mcfoster022@gmail.com>
2023-01-13 00:02:13 -0500

    Adding card view

---
+++ Shared/Models/CardView.swift
@@ -0,0 +1,33 @@
+//
+//  CardView.swift
+//  scrumdinger (iOS)
+//
+//  Created by Maria Foster on 1/12/23.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    let scrum: DailyScrum
+    var body: some View {
+        VStack{
+            Text(scrum.title).font(.headline)
+            Spacer()
+            HStack{
+                Label("\(scrum.attendees.count)", systemImage: "person.3")
+            }
+
+        }
+    }
+}
+
+struct CardView_Previews:
+    PreviewProvider {
+    static var scrum = DailyScrum.sampleData[0]
+    static var previews: some View {
+        CardView(scrum: scrum)
+            .background(scrum.theme.mainColor)
+            .previewLayout(.fixed(width: 400, height: 60))
+        
+    }
+}

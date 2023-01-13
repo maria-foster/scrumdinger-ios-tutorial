6c56b6923d72a7398e2d56cf57cf699e45d6fa3b
Maria Foster <mcfoster022@gmail.com>
2023-01-12 23:50:34 -0500

    Adding DailyScrum struct

---
+++ Shared/Models/DailyScrum.swift
@@ -0,0 +1,15 @@
+//
+//  DailyScrum.swift
+//  scrumdinger (iOS)
+//
+//  Created by Maria Foster on 1/12/23.
+//
+
+import Foundation
+
+struct DailyScrum {
+    var title : String
+    var attendees:[String]
+    var lengthInMinutes: Int
+    var theme: Theme
+}

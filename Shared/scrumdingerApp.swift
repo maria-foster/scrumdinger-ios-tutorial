2537592a4e86ea8da9beb332ff1d7a362e053317
Maria Foster <mcfoster022@gmail.com>
2023-01-12 23:02:01 -0500

    Initial Commit

---
+++ Shared/scrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  scrumdingerApp.swift
+//  Shared
+//
+//  Created by Maria Foster on 1/12/23.
+//
+
+import SwiftUI
+
+@main
+struct scrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MeetingView()
+        }
+    }
+}
